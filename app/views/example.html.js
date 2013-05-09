9c2ef152479619945b0b97a34f457f0d5c41a02b
Brian McLaughlin <bmclaugh@redhat.com>
2013-05-08 22:22:40 -0400

    Reformatted SCSS, added spacers and dividers, added responsive tweaks for 480px or less wide displays

--- app/views/example.html.js
+++ app/views/example.html.js
@@ -2,333 +2,98 @@ angular.module("alch-templates").run(function($templateCache) {
   $templateCache.put("app/views/example.html",
     "<section class=\"content\">" +
     "" +
-    "<h3>HTML Types</h3>" +
-    "" +
     "<div class=\"btn-toolbar\">" +
+    "  <button class=\"btn\">Default</button>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
     "  <div class=\"btn-group\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
+    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
     "  </div>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <button class=\"btn\">Default</button>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <div class=\"btn-group\">" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
+    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
+    "  </div>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <button class=\"btn\">Default</button>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <div class=\"btn-group\">" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
+    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
+    "  </div>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <button class=\"btn\">Default</button>" +
     "</div>" +
     "" +
     "<br/>" +
     "" +
     "<div class=\"btn-toolbar\">" +
+    "  <button class=\"btn pull-left\">Default 1</button>" +
+    "  <span class=\"spacer mini pull-left\"></span>" +
     "  <div class=\"btn-group pull-left\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
-    "  </div><div class=\"btn-group pull-right\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
+    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
     "  </div>" +
+    "  <button class=\"btn pull-right\">Default 3</button>" +
+    "  <div class=\"btn-group pull-right\">" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
+    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
+    "  </div>" +
+    "  <button class=\"btn pull-right\">Default 2</button>" +
     "</div>" +
     "" +
     "<br/>" +
     "" +
     "<div class=\"btn-toolbar btn-toolbar-vertical\">" +
+    "  <button class=\"btn\">Default</button>" +
+    "  <button class=\"btn primary\">Primary</button>" +
+    "  <button class=\"btn warning\">Warning</button>" +
+    "  <span class=\"spacer huge\"></span>" +
+    "  <span class=\"divider centered\"></span>" +
+    "  <span class=\"spacer huge\"></span>" +
     "  <div class=\"btn-group\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a href=\"#\" class=\"btn\">Link</a>" +
-    "    <button>Button</button>" +
-    "    <input class=\"btn\" type=\"reset\" value=\"Reset\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Submit\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<h3>Features - Sizes</h3>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group pull-left\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div><div class=\"btn-group pull-right\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar btn-toolbar-vertical\">" +
-    "  <div class=\"btn-group\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn mini\" type=\"submit\" value=\"Mini\"/>" +
-    "    <input class=\"btn small\" type=\"submit\" value=\"Small\"/>" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn large\" type=\"submit\" value=\"Large\"/>" +
-    "    <input class=\"btn huge\" type=\"submit\" value=\"Huge\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<h3>Features - Colors</h3>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
     "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
     "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group pull-left\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div><div class=\"btn-group pull-right\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <div class=\"btn-group\">" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
     "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
     "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar btn-toolbar-vertical\">" +
+    "  <span class=\"spacer\"></span>" +
+    "  <span class=\"divider\"></span>" +
+    "  <span class=\"spacer\"></span>" +
     "  <div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Default\"/>" +
-    "    <input class=\"btn primary\" type=\"submit\" value=\"Primary\"/>" +
+    "    <a class=\"btn\">Default</a>" +
+    "    <button class=\"btn primary\">Primary</button>" +
     "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
     "  </div>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <span class=\"divider centered\"></span>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <button class=\"btn\">Default</button>" +
+    "  <button class=\"btn primary\">Primary</button>" +
+    "  <button class=\"btn warning\">Warning</button>" +
+    "  <span class=\"spacer\"></span>" +
+    "  <span class=\"divider centered\"></span>" +
+    "  <span class=\"spacer\"></span>" +
     "</div>" +
-    "" +
-    "<h3>Features - Statuses</h3>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group pull-left\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div><div class=\"btn-group pull-right\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar btn-toolbar-vertical\">" +
-    "  <div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <input class=\"btn\" type=\"submit\" value=\"Disabled\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "    <input class=\"btn processing\" type=\"submit\" value=\"Processing\" title=\"We are working on that...\" disabled=\"disabled\"/>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<h3>Features - Icons</h3>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <div class=\"btn-group pull-left\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "  </div><div class=\"btn-group pull-right\">" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "<div class=\"btn-toolbar btn-toolbar-vertical\">" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div><div class=\"btn-group\">" +
-    "    <a class=\"btn icon\" href=\"#\">Download link (:before)</a>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <a class=\"btn\" href=\"#\">Download link (&lt;i&gt;)</a>" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (:before)\">" +
-    "    </div>" +
-    "    <div class=\"btn icon\">" +
-    "      <i class=\"download_icon-black\"></i>" +
-    "      <input class=\"btn\" type=\"submit\" value=\"Submit (<i>)\">" +
-    "    </div>" +
-    "  </div>" +
-    "</div>" +
-    "" +
-    "<br/>" +
-    "" +
-    "</section>");
+    "");
 });

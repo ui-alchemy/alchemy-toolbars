1cf332de628bc13b527471dd4772492936b299bb
Jaromir Coufal <jcoufal@redhat.com>
2013-05-10 23:23:18 +0200

    2nd round of styling, enhanced example to show all the functionality of toolbars

--- app/views/example.html.js
+++ app/views/example.html.js
@@ -1,109 +1,67 @@
 angular.module("alch-templates").run(function($templateCache) {
   $templateCache.put("app/views/example.html",
-    "<section class=\"content\">" +
+    "<div class=\"toolbar\">" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
     "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <button class=\"btn\">Default</button>" +
     "  <span class=\"divider\"></span>" +
+    "" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
+    "" +
     "  <span class=\"spacer\"></span>" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div>" +
-    "  <span class=\"divider\"></span>" +
-    "  <span class=\"spacer\"></span>" +
-    "  <button class=\"btn\">Default</button>" +
-    "  <span class=\"divider\"></span>" +
-    "  <span class=\"spacer\"></span>" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div>" +
-    "  <span class=\"divider\"></span>" +
-    "  <span class=\"spacer\"></span>" +
-    "  <button class=\"btn\">Default</button>" +
-    "  <span class=\"divider\"></span>" +
-    "  <span class=\"spacer\"></span>" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div>" +
-    "  <span class=\"divider\"></span>" +
-    "  <span class=\"spacer\"></span>" +
-    "  <button class=\"btn\">Default</button>" +
+    "" +
+    "  <button class=\"btn\">button</button>" +
+    "" +
+    "  <button class=\"btn pull_right\">button</button>" +
+    "  <button class=\"btn pull_right\">button</button>" +
     "</div>" +
     "" +
     "<br/>" +
     "" +
-    "<div class=\"btn-toolbar\">" +
-    "  <button class=\"btn pull-left\">Default 1</button>" +
-    "  <span class=\"spacer mini pull-left\"></span>" +
-    "  <div class=\"btn-group pull-left\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
+    "<div class=\"toolbar\">" +
+    "  <button class=\"btn pull_left\">button</button>" +
+    "  <button class=\"btn pull_left\">button</button>" +
+    "" +
+    "  <div class=\"btn_group pull_left\">" +
+    "    <button class=\"btn\">button</button>" +
+    "    <button class=\"btn\">button</button>" +
+    "    <button class=\"btn\">button</button>" +
     "  </div>" +
-    "  <button class=\"btn pull-right\">Default 3</button>" +
-    "  <div class=\"btn-group pull-right\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
+    "" +
+    "  <div class=\"btn_group pull_left\">" +
+    "    <button class=\"btn\">button</button>" +
+    "    <button class=\"btn\">button</button>" +
+    "    <button class=\"btn\">button</button>" +
     "  </div>" +
-    "  <button class=\"btn pull-right\">Default 2</button>" +
+    "" +
+    "  <div class=\"btn_group pull_right\">" +
+    "    <button class=\"btn\">button</button>" +
+    "    <button class=\"btn\">button</button>" +
+    "    <button class=\"btn\">button</button>" +
+    "  </div>" +
+    "" +
+    "    <button class=\"btn pull_right\">button</button>" +
     "</div>" +
     "" +
     "<br/>" +
     "" +
-    "<div class=\"btn-toolbar btn-toolbar-vertical\">" +
-    "  <button class=\"btn\">Default</button>" +
-    "  <button class=\"btn primary\">Primary</button>" +
-    "  <button class=\"btn warning\">Warning</button>" +
+    "<div class=\"toolbar vertical\">" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
     "" +
-    "  <span class=\"spacer huge\"></span>" +
-    "  <span class=\"divider centered\"></span>" +
-    "  <span class=\"spacer huge\"></span>" +
+    "  <span class=\"divider\"></span>" +
     "" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div>" +
+    "  <button class=\"btn\">button</button>" +
     "" +
     "  <span class=\"spacer\"></span>" +
-    "  <span class=\"divider centered\"></span>" +
-    "  <span class=\"spacer\"></span>" +
     "" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div>" +
-    "" +
-    "  <span class=\"spacer\"></span>" +
-    "  <span class=\"divider centered\"></span>" +
-    "  <span class=\"spacer\"></span>" +
-    "" +
-    "  <div class=\"btn-group\">" +
-    "    <a class=\"btn\">Default</a>" +
-    "    <button class=\"btn primary\">Primary</button>" +
-    "    <input class=\"btn warning\" type=\"submit\" value=\"Warning\"/>" +
-    "  </div>" +
-    "" +
-    "  <span class=\"spacer huge\"></span>" +
-    "  <span class=\"divider centered\"></span>" +
-    "  <span class=\"spacer huge\"></span>" +
-    "" +
-    "  <button class=\"btn\">Default</button>" +
-    "  <button class=\"btn primary\">Primary</button>" +
-    "  <button class=\"btn warning\">Warning</button>" +
-    "" +
-    "  <span class=\"spacer huge\"></span>" +
-    "  <span class=\"divider centered\"></span>" +
-    "  <span class=\"spacer huge\"></span>" +
-    "" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
+    "  <button class=\"btn\">button</button>" +
     "</div>" +
     "");
 });
